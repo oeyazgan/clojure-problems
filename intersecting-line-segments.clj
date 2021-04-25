976194e3066fd4380342c540815c936d24021f6b
oeyazgan <onuremreyazgan@gmail.com>
2021-04-25 19:34:11 +0300

    more functional

--- intersecting-line-segments.clj
+++ intersecting-line-segments.clj
@@ -16,23 +16,19 @@
 
 (defn is-first-to-increment [x y]
   "determine the array to get tail of"
-  (if (< (get x 1) (get y 1))
-    true
-    false))
+  (< (second x) (second y)))
 
 (defn get-all-intersections [first-segments second-segments intersections]
   "check intersecting line segments in linear time complexity"
   (if (or (empty? first-segments) (empty? second-segments))
     (remove empty? intersections)
-    (if (is-first-to-increment (first first-segments) (first second-segments))
-      (get-all-intersections (rest first-segments) second-segments
-                             (conj intersections (get-intersections (first first-segments) (first second-segments))))
-      (get-all-intersections first-segments (rest second-segments)
-                             (conj intersections (get-intersections (first first-segments) (first second-segments)))))
+    (let [new-intersections (conj intersections (get-intersections (first first-segments) (first second-segments)))]
+      (if (is-first-to-increment (first first-segments) (first second-segments))
+        (get-all-intersections (rest first-segments) second-segments new-intersections)
+        (get-all-intersections first-segments (rest second-segments) new-intersections)))
     ))
 
 (defn -main
   [& args]
   (println (get-all-intersections [[1 2] [3 6] [9 1000]] [[1 3] [5 7] [700 800] [850 861]] [])))
-
 
