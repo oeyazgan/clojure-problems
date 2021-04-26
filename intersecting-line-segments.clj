3a8abd5d1fc92f0a92de64b24bd59f986147bca2
oeyazgan <onuremreyazgan@gmail.com>
2021-04-27 00:40:20 +0300

    multi arity

--- intersecting-line-segments.clj
+++ intersecting-line-segments.clj
@@ -18,17 +18,21 @@
   "determine the array to get tail of"
   (< (second x) (second y)))
 
-(defn get-all-intersections [first-segments second-segments intersections]
-  "check intersecting line segments in linear time complexity"
-  (if (or (empty? first-segments) (empty? second-segments))
-    (remove empty? intersections)
-    (let [new-intersections (conj intersections (get-intersections (first first-segments) (first second-segments)))]
-      (if (is-first-to-increment (first first-segments) (first second-segments))
-        (get-all-intersections (rest first-segments) second-segments new-intersections)
-        (get-all-intersections first-segments (rest second-segments) new-intersections)))
-    ))
+(defn get-all-intersections
+  ([first-segments second-segments]
+  (get-all-intersections first-segments second-segments []))
+  ([first-segments second-segments intersections]
+   "check intersecting line segments in linear time complexity"
+   (if (or (empty? first-segments) (empty? second-segments))
+     (remove empty? intersections)
+     (let [new-intersections (conj intersections (get-intersections (first first-segments) (first second-segments)))]
+       (if (is-first-to-increment (first first-segments) (first second-segments))
+         (get-all-intersections (rest first-segments) second-segments new-intersections)
+         (get-all-intersections first-segments (rest second-segments) new-intersections)))
+     )))
 
 (defn -main
   [& args]
-  (println (get-all-intersections [[1 2] [3 6] [9 1000]] [[1 3] [5 7] [700 800] [850 861]] [])))
+  (println (get-all-intersections [[1 2] [3 6] [9 1000]] [[1 3] [5 7] [700 800] [850 861]])))
+
 
