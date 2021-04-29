8b9dbf2a535e71c4753666529c09d7243bc18783
oeyazgan <onuremreyazgan@gmail.com>
2021-04-29 21:52:52 +0300

    add multi func support

--- intersecting-line-segments.clj
+++ intersecting-line-segments.clj
@@ -19,8 +19,10 @@
   (< (second x) (second y)))
 
 (defn get-all-intersections
+  ([all-segments]
+   (get-all-intersections (first all-segments) (second all-segments)))
   ([first-segments second-segments]
-  (get-all-intersections first-segments second-segments []))
+   (get-all-intersections first-segments second-segments []))
   ([first-segments second-segments intersections]
    "check intersecting line segments in linear time complexity"
    (if (or (empty? first-segments) (empty? second-segments))
@@ -31,8 +33,33 @@
          (get-all-intersections first-segments (rest second-segments) new-intersections)))
      )))
 
+(defn in? 
+  [coll elm]  
+  (some #(= elm %) coll))
+
+(defn custom-split [my-vec character] 
+  [(subvec my-vec 0 (.indexOf my-vec character)) (subvec my-vec (+ 1 (.indexOf my-vec character)))]) 
+
+(defn dispatch-input-format 
+  ([inp]
+   (cond  
+     (contains? inp :first) :inp-map
+     (reduce #(and %1 %2) [(vector? (first inp)) (vector? (second inp)) ((complement in?) inp "|")]) :inp-standard 
+     (and (vector? inp) (in? inp "|")) :inp-vector)
+   ))
+
+(defmulti normalize-input dispatch-input-format)
+(defmethod normalize-input :inp-standard [inp] inp)
+(defmethod normalize-input :inp-map [inp]
+  (vector (get inp :first) (get inp :second)))
+(defmethod normalize-input :inp-vector [inp] (custom-split inp "|"))
+
 (defn -main
   [& args]
-  (println (get-all-intersections [[1 2] [3 6] [9 1000]] [[1 3] [5 7] [700 800] [850 861]])))
-
+  (def my-standard-vec [[[1 2] [3 6] [9 1000]] [[1 3] [5 7] [700 800] [850 861]]]) 
+  (def my-vec [[1 2] [3 6] [9 1000] "|" [1 3] [5 7] [700 800] [850 861]]) 
+  (def my-map {:first [[1 2] [3 6] [9 1000]] :second [[1 3] [5 7] [700 800] [850 861]]}) 
+  (println (get-all-intersections (normalize-input my-standard-vec)))
+  (println (get-all-intersections (normalize-input my-vec)))
+  (println (get-all-intersections (normalize-input my-map))))
 
