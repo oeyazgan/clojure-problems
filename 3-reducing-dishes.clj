22f17f5332c0a1c4916328ab20f135e55cd47ba4
oeyazgan <onuremreyazgan@gmail.com>
2021-05-09 16:27:14 +0300

    refactor

--- 3-reducing-dishes.clj
+++ 3-reducing-dishes.clj
@@ -3,17 +3,15 @@
 
 (defn should-keep? 
   [[first & rest]]
-  (cond (and (empty? rest) (pos? first))
-        true
-        :else (< (Math/abs first) (reduce + rest))))
+  (or (and (empty? rest) (pos? first))
+      (< (Math/abs first) (reduce + rest))))
 
 (defn reducing-dishes 
   ([index total satisfaction-sorted]
-  (cond (empty? satisfaction-sorted) total
-        (should-keep? satisfaction-sorted)
-        (reducing-dishes (+ index 1) (+ total (* index (first satisfaction-sorted))) (rest satisfaction-sorted))
-        :else (reducing-dishes index total (rest satisfaction-sorted))))) 
+   (cond (empty? satisfaction-sorted) total
+         (should-keep? satisfaction-sorted)
+         (reducing-dishes (inc index) (+ total (* index (first satisfaction-sorted))) (rest satisfaction-sorted))
+         :else (reducing-dishes index total (rest satisfaction-sorted))))) 
 
-(defn reducing-dishes-wrapper 
-  [satisfaction]
-  (reducing-dishes 1 0 (sort satisfaction)))
+(def reducing-dishes-wrapper 
+  (comp (partial reducing-dishes 1 0) sort))
