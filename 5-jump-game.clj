2385688ff616fed1c17ce4f89c2af75413f63595
yazgan <onur.yazgan@englishcentral.com>
2021-05-29 16:25:08 +0300

    debug and changes for more idiomatic clj

--- 5-jump-game.clj
+++ 5-jump-game.clj
@@ -1,16 +1,24 @@
 (ns ahmet-buhanla-zihinden-clojure.core
   (:gen-class))
 
-(defn find-index-max [vec]
-  (first (apply max-key second (map-indexed vector vec))))
+(defn find-optimal-index [vec]
+  (let [indexed-vec (map-indexed vector vec)
+        vec-index-added-val (map #(vector (first %) (reduce + %)) indexed-vec)
+        elem-optimal-jump (apply max-key second vec-index-added-val)]
+    (first elem-optimal-jump)))
 
 (defn possible-jumps [vec]
-  (subvec vec 1 (inc (first vec))))
-
-(defn find-index-max-ps-jump [vec]
-  (inc (find-index-max (possible-jumps vec)))
+  (let [last-index (count vec)
+        max-jump-range (->> vec first inc)]
+    (subvec vec 1 (min last-index max-jump-range))))
+  
+(defn find-index-opt-ps-jump [vec]
+  (->> vec
+    possible-jumps
+    find-optimal-index
+    inc))
 
 (defn jump-game [vec]
   (cond (= (first vec) 0) false
         (= (count vec) 1) true
-	:else (jump-game (subvec vec (find-index-max-ps-jump vec)))))
+	:else (jump-game (subvec vec (find-index-opt-ps-jump vec)))))
