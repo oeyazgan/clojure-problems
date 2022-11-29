848dd593672afcba9bb3767b6d11d2a627c90676
oeyazgan <onuremreyazgan@gmail.com>
2022-11-29 03:07:14 +0100

    remove unneccessary cond

--- 6-pascal-triangle.clj
+++ 6-pascal-triangle.clj
@@ -3,12 +3,10 @@
 
 (defn pascal-triangle [x]
   (cond (= x 1) [1]
-        (= x 2) [1 1]
         :else (let [start [1]
                     end [1]
                     middle (map #(apply + %) (partition 2 1 x))]
                 (concat start middle end))))
-
 
 (def all-pascal-triangles (iterate pascal-triangle [1]))
 
