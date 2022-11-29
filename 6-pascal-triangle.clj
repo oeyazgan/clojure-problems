8240bcb964df111d0030bba7092335df08d0ac8b
Onur <63983964+oeyazgan@users.noreply.github.com>
2022-11-29 02:57:45 +0100

    add pascal triangle with lazy seqs

---
+++ 6-pascal-triangle.clj
@@ -0,0 +1,15 @@
+(ns ahmet-buhanla-zihinden-clojure.core
+  (:gen-class))
+
+(defn pascal-triangle [x]
+  (cond (= x 1) [1]
+        (= x 2) [1 1]
+        :else (let [start [1]
+                    end [1]
+                    middle (map #(apply + %) (partition 2 1 x))]
+                (concat start middle end))))
+
+
+(def all-pascal-triangles (iterate pascal-triangle [1]))
+
+(take 30 all-pascal-triangles)
