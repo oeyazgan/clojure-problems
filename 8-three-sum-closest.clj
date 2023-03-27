dbf6104420a1d55059c45c0b247c81360dc0d65a
yazgan <onuremreyazgan@gmail.com>
2023-03-27 19:39:56 -0400

    add reusable combination calculator

--- 8-three-sum-closest.clj
+++ 8-three-sum-closest.clj
@@ -1,32 +1,15 @@
 (ns ahmet-buhanla-zihinden-clojure.core
   (:gen-class))
 
-(defn get-three-combs [l]
-  (let [size (count l)]
-    (loop [first-index 0
-           second-index 1
-           rst (subvec l (+ 2 first-index))
-           combs []]
-      (cond
-        (= first-index (- size 3)) (conj combs (vec (take-last 3 l)))
-        (= second-index (- size 2)) (recur (inc first-index)
-                                           (+ 2 first-index)
-                                           (subvec l (+ 3 first-index))
-                                           combs)
-        (empty? rst) (recur first-index
-                            (inc second-index)
-                            (subvec l (+ 2 second-index))
-                            combs)
-        :else (recur first-index
-                     second-index
-                     (rest rst)
-                     (conj combs [(nth l first-index)
-                                  (nth l second-index)
-                                  (first rst)]))))))
+(defn get-subsets [n [f & r :as all]]
+  (cond (= n 0) [[]]
+        (empty? all) []
+        :else (concat (map #(cons f %) (get-subsets (dec n) r))
+                      (get-subsets n r))))
 
 (defn three-sum-closest [l target]
   (->> l
-       get-three-combs
+       (get-subsets 3)
        (map #(apply + %))
        (#(for [i %] {:sum i :diff (abs (- i target))}))
        (sort-by :diff)
@@ -35,12 +18,16 @@
 
 (three-sum-closest [-1 2 1 -4] 1)
 ;; => 2
+;; => 2
 
 (three-sum-closest [1 1 1 0] -100)
+;; => 2
 ;; => 2
 
 (three-sum-closest [4,0,5,-5,3,3,0,-4,-5] -2)
 ;; => -2
+;; => -2
 
 (three-sum-closest [4,0,5,3,3,0,-4] -2)
+;; => -1
 ;; => -1
